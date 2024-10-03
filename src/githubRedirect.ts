1bf37f6a93ddc9cc62d6ba85ab10c6a17ef0f5a5
Josh Morton <josh.morton@propeltech.co.uk>
2024-10-03 20:53:49 +0100

    redirecter fix in src

--- src/githubRedirect.ts
+++ src/githubRedirect.ts
@@ -3,7 +3,8 @@
     const decoded = l.search.slice(1).split('&').map(function(s) { 
       return s.replace(/~and~/g, '&')
     }).join('?');
-    window.history.replaceState(null, null,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    window.history.replaceState(null, null as any,
         l.pathname.slice(0, -1) + decoded + l.hash
     );
   }
